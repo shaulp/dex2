71512e9c118a4936f00e047666d94400dc7c0ff4
Shaul Peled <shaul@peled.cc>
2014-03-17 16:00:56 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dex2::Application.config.secret_key_base = '9c327a7d41f3a7db2feea791d3ff702ae52976d0ab2ad7a160d3d0931bfa03062e4bb032ea2740d4fd6ec05c1b29261f5ffc83db5ed14e6da37d455c53093a91'
